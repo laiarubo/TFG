79fdb91fde61f2e60ae24d9ef53b02191d97f216
Laia <laia@users.noreply.github.com>
2021-01-30 14:24:57 +0100

    Biofeedback - Quadern de bitàcola
    - Comunicació PC<-->Arduino automàtica
    - Es mostra data i hora
    - Es poden escriure notes al quadern de bitàcola (es mostra l'hora d'enviament i "Nota: " + la nota)

---
+++ _Projecte_/Biofeedback/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Biofeedback
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.NotesTerapeuta = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.QuadernDeBitacola = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hora";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Data";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // NotesTerapeuta
+            // 
+            this.NotesTerapeuta.Location = new System.Drawing.Point(16, 411);
+            this.NotesTerapeuta.Name = "NotesTerapeuta";
+            this.NotesTerapeuta.Size = new System.Drawing.Size(286, 20);
+            this.NotesTerapeuta.TabIndex = 3;
+            this.NotesTerapeuta.TextChanged += new System.EventHandler(this.NotesTerapeuta_TextChanged);
+            this.NotesTerapeuta.KeyDown += new System.Windows.Forms.KeyEventHandler(this.NotesTerapeuta_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Envia";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(95, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(269, 69);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "LEDtemp";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // QuadernDeBitacola
+            // 
+            this.QuadernDeBitacola.Location = new System.Drawing.Point(16, 247);
+            this.QuadernDeBitacola.Multiline = true;
+            this.QuadernDeBitacola.Name = "QuadernDeBitacola";
+            this.QuadernDeBitacola.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.QuadernDeBitacola.Size = new System.Drawing.Size(334, 157);
+            this.QuadernDeBitacola.TabIndex = 7;
+            this.QuadernDeBitacola.TextChanged += new System.EventHandler(this.QuadernDeBitacola_TextChanged);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.QuadernDeBitacola);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.NotesTerapeuta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox NotesTerapeuta;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox QuadernDeBitacola;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ _Projecte_/Biofeedback/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+using System.Windows.Forms;
+using System.IO.Ports;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace Biofeedback
+{
+    public partial class Form1 : Form
+    {
+        // ======= VARIABLES GLOBALS =======
+
+        // Per a la connexió PC - Arduino
+        bool CONNECTAT = false;
+        string[] PORTS;
+        SerialPort PORT;
+
+        // Per al quadern de bitàcola
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string TEXT_REBRE;
+        public string TEXT_ENVIAR;
+        // =================================
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            PORTS = SerialPort.GetPortNames();
+            
+            foreach (string PORT in PORTS)
+            {
+                comboBox1.Items.Add(PORT); // Es llisten els ports COM disponibles
+                Console.WriteLine(PORT);
+                if (PORTS[0] != null)
+                {
+                    comboBox1.SelectedItem = PORTS[0];
+                }
+            }
+
+            // Connexió PC - Arduino automàtica
+            if (!CONNECTAT)
+            {
+                connectarArduino();
+            }
+
+            // Quadern de bitàcola
+       //     backgroundWorker1.RunWorkerAsync(); // L'aplicació llista els events
+            backgroundWorker1.WorkerSupportsCancellation = true; // El terapeuta publica notes
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label1.Text = DateTime.Now.ToShortDateString();
+            label2.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // Perquè s'actualitzin els segons de l'hora
+        {
+            label2.Text = DateTime.Now.ToLongTimeString();
+            timer1.Start();
+        }
+
+        private void connectarArduino()
+        {
+            CONNECTAT = true;
+            string portSeleccionat = comboBox1.GetItemText(comboBox1.SelectedItem);
+
+            // public SerialPort (string portName, int baudRate, System.IO.Ports.Parity parity);
+            PORT = new SerialPort(portSeleccionat, 9600, Parity.None, 8, StopBits.One);
+
+            PORT.Open(); // S'activa el port COM
+            PORT.Write("#INICI\n");
+        }
+
+
+        private void NotesTerapeuta_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CONNECTAT)
+            {
+                if (checkBox1.Checked)
+                {
+                    PORT.Write("#LED_ON\n");
+                }
+                else
+                {
+                    PORT.Write("#LED_OFF\n");
+                }
+            }
+        }
+
+        private void reiniciarQC()
+        {
+            checkBox1.Checked = false;
+        }
+
+        private void c_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (CONNECTAT)
+            {
+                this.QuadernDeBitacola.Invoke(new MethodInvoker(delegate ()
+                {
+                    QuadernDeBitacola.AppendText(DateTime.Now.ToShortTimeString() + "    Nota: " + TEXT_ENVIAR + System.Environment.NewLine);
+                }));
+            }
+            else
+            {
+                MessageBox.Show("Hi ha algun error. No es pot enviar la nota.");
+            }
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (NotesTerapeuta.Text != "")
+            {
+                TEXT_ENVIAR = NotesTerapeuta.Text;
+                backgroundWorker1.RunWorkerAsync();
+            }
+            NotesTerapeuta.Text = "";
+        }
+
+        private void NotesTerapeuta_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //button1_Click(this, new EventArgs()); // Fa el mateix que la línia següent:
+                button1.PerformClick();
+            }
+        }
+
+        private void QuadernDeBitacola_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
